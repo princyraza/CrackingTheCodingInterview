0fb4a4fc50bcd8126f68cbfa06113c3cee6b98ed
princy <princy.raza@gmail.com>
2018-10-25 07:38:20 +0200

    add bfs

--- src/com/princylabs/chapter4/Chapter4.java
+++ src/com/princylabs/chapter4/Chapter4.java
@@ -1,4 +1,24 @@
 package com.princylabs.chapter4;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Chapter4 {
+
+    public void BFS(int v, Graph graph) {
+        LinkedList<Integer> nextToVisit = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        nextToVisit.add(v);
+
+        while(!nextToVisit.isEmpty()) {
+            int node = nextToVisit.remove();
+            visited.add(node);
+            System.out.println("Visiting node " + node);
+            for (int n : graph.adj(node)) {
+                if(!visited.contains(n)) {
+                    nextToVisit.add(n);
+                }
+            }
+        }
+    }
 }
